9dbfc13d1416512b7ac39351e73cf6af0b6d739a
bayudeveloper <anugrahabayu4@gmail.com>
2026-03-04 10:04:50 +0700

    Update sora2.js

--- src/api/ai/sora2.js
+++ src/api/ai/sora2.js
@@ -218,7 +218,7 @@ module.exports = function(app) {
             return res.status(400).json({
                 status:  false,
                 message: "Parameter 'prompt' wajib diisi!",
-                contoh:  '/ai/sora?prompt=a cat walking&apikey=M0NPI'
+                contoh:  '/ai/sora?prompt=a cat walking&apikey=admin277'
             });
         }
 
